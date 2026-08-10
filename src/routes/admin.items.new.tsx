cda5688293df8516eafe9d1c9a17183b9ccb46cd
Milan <frolov.milan@gmail.com>
2026-08-10 06:02:18 +0300

    remove admin pannel

--- src/routes/admin.items.new.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { ItemForm } from "@/components/item-form";
-
-export const Route = createFileRoute("/admin/items/new")({
-  component: () => <ItemForm />,
-});
